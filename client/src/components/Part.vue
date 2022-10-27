e3cc337288eff3ed592284512d6c3e71723c018c
Matthew DeRoxtro (Linux) <mderoxtro@gmail.com>
2022-10-27 14:31:09 -0400

    update parts

---
+++ client/src/components/Part.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="part">
+  </div>
+</template>
+
+<style scoped>
+.part {
+  display: flex;
+  min-height: 100vh;
+  margin-left: auto;
+  margin-right: auto;
+  width: 1000px;
+  gap: 10px;
+}
+</style>
--- client/src/views/PartsView.vue
+++ client/src/views/PartsView.vue
@@ -1,8 +1,24 @@
 <template>
 <div class="parts">
   <h1>Parts</h1>
-    <div>{{ state }}</div>
-    <p>Parts Go Here</p>
+    <div class="container">
+    <div v-for="item in state" class="partbox" part="item">
+    <p>PHOTO</p>
+    <p>3DeRox #: {{ item.deroxNum }}</p>
+    <p>Manufacturer #: {{ item.manNum }}</p>
+    <p>Description: {{ item.description }}</p>
+    <p>Materials: <li v-for="mat in item.material">{{ mat }}</li></p>
+    <p>Colors: <li v-for="col in item.colors">{{ col }}</li></p>
+    <p>Makes: <li v-for="mak in item.makes">{{ mak }}</li></p>
+    <p>Models: <li v-for="mod in item.models">{{ mod }}</li></p>
+    <p>Years: <li v-for="yea in item.years">{{ yea }}</li></p>
+    <p>Required Quantity: {{ item.reqQty }}</p>
+    <p>Price: $ {{ item.price }}</p>
+    <p>Storefronts: <li v-for="store in item.storefronts">{{ store }}</li></p>
+    <p>Additional Files: {{ item.addFiles }}</p>
+    <button @click='viewPart()'>View Part Details</button>
+    </div>
+    </div>
 </div>
 </template>
 
@@ -16,6 +32,18 @@
     gap: 10px;
     flex-direction: column;
   }
+  .container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
+  }
+  .partbox {
+    display: flex;
+    flex-direction: column;
+    min-width: 200px;
+  }
   h1 {
     display: flex;
     justify-content: center;
@@ -26,6 +54,14 @@
   import { ref } from 'vue'
   
   export default {
+    props: {
+      part: Object
+    },
+    methods: {
+      viewPart() {
+        this.$emit('passPart')
+      }
+    },
     setup() {
       const state = ref({})
       
